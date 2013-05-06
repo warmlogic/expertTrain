8d1cd642ce8c099bcf5cb15513618e38121eecd7
Matt Mollison <matt.mollison@gmail.com>
2013-05-06 17:16:50 -0600

    Small bug fix

--- et_shuffleStims.m
+++ et_shuffleStims.m
@@ -5,23 +5,23 @@ function [shuffledStims] = et_shuffleStims(stims,valueField,maxConsec)
 %  stims:      Struct. Stimuli to shuffle. Assumes that the field
 %              stims.(valueField) consists of integers.
 %  valueField: String. Name of the field on which the order is contingent.
-%  maxConsec:  Integer. Maximum number of consecutive stimuli with the same
-%              value.
+%  maxConsec:  Integer. Maximum number of consecutive stimuli from the same
+%              family.
 %
 % Output:
 %  shuffledStims: Stimuli in shuffled order.
 %
-% NB: Makes 1000 shuffle attempts before erroring.
+% NB: Makes 10000 shuffle attempts before erroring.
 %
 
 not_good = true;
-maxShuffle = 1000;
+maxShuffle = 10000;
 shuffleCount = 0;
 while not_good
-  %randsel = randperm(length(stims));
+  randsel = randperm(length(stims));
   % debug
-  randsel = 1:length(stims);
-  fprintf('%s, NB: Debug code. Not actually randomizing!\n',mfilename);
+  %randsel = 1:length(stims);
+  %fprintf('%s, NB: Debug code. Not actually randomizing!\n',mfilename);
   % shuffle the exemplars
   stims = stims(randsel);
   
@@ -36,7 +36,6 @@ while not_good
       % if we found a repeat, add 1 to the count
       consecCount(stimValues(i)) = consecCount(stimValues(i)) + 1;
       if consecCount(stimValues(i)) > maxConsec
-        fprintf('omg\n');
         % if we hit the maximum number, break out
         break
       end
@@ -46,15 +45,17 @@ while not_good
       consecCount(stimValues(i)) = 1;
     end
   end
-  if any(consecCount) > maxConsec
+  if any(consecCount > maxConsec)
     shuffleCount = shuffleCount + 1;
+    fprintf('.');
   else
     not_good = false;
     shuffledStims = stims;
+    fprintf('\nSuccessfully shuffled the stimuli contingent on the %s field.\n',valueField);
   end
   
   if shuffleCount == maxShuffle
-    error('Performed %d shuffle attempts. That is too many.',maxShuffle);
+    error('\nPerformed %d shuffle attempts. That is too many.',maxShuffle);
   end
 end % while
 
