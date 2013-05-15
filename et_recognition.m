eaf9dfaa15bf5614c8057a91b537e8501a8db622
Matt Mollison <matt.mollison@gmail.com>
2013-05-15 14:16:51 -0600

    Recognition task is pretty much done

--- et_recognition.m
+++ et_recognition.m
@@ -27,17 +27,13 @@ fprintf('Running recognition task for %s %s...\n',sesName,phase);
 % TODO:
 %  make instruction files. read in during config?
 
-% using these
+% % using these
 % cfg.keys.recogKeyNames
 % cfg.keys.recogDefUn
 % cfg.keys.recogMayUn
 % cfg.keys.recogMayF
 % cfg.keys.recogDefF
 % cfg.keys.recogRecoll
-
-% % not using these
-% cfg.keys.recogOld
-% cfg.stim.recogNew
 
 % % durations, in seconds
 % cfg.stim.(sesName).(phase).study_isi = 0.8;
@@ -46,9 +42,6 @@ fprintf('Running recognition task for %s %s...\n',sesName,phase);
 % cfg.stim.(sesName).(phase).test_isi = 0.8;
 % cfg.stim.(sesName).(phase).test_preStim = 0.2;
 % cfg.stim.(sesName).(phase).test_stim = 1.5;
-% % TODO: do we need response?
-% cfg.stim.(sesName).(phase).response = 1.5;
-
 
 % % debug
 % sesName = 'pretest';
@@ -56,31 +49,30 @@ fprintf('Running recognition task for %s %s...\n',sesName,phase);
 
 phaseCfg = cfg.stim.(sesName).(phase);
 
-% % % Don't actually need to check this
-% % make sure we have enough stimuli
-% if (phaseCfg.nBlocks * phaseCfg.nTargPerBlock) > phaseCfg.nBlocks * length(expParam.session.(sesName).(phase).targStims{1})
-%   error('Not enough target stimuli per study block!');
-% end
-% if (phaseCfg.nBlocks * phaseCfg.nLurePerBlock) > phaseCfg.nBlocks * length(expParam.session.(sesName).(phase).lureStims{1})
-%   error('Not enough lure stimuli per test block!');
-% end
-
-% only check these keys
-RestrictKeysForKbCheck([cfg.keys.recogDefUn, cfg.keys.recogMayUn, cfg.keys.recogMayF, cfg.keys.recogDefF, cfg.keys.recogRecoll]);
+% read the proper response key image
+testRespImgFile = fullfile(cfg.files.resDir,sprintf('recog_test_resp%d.jpg',cfg.keys.recogKeySet));
+testRespImg = imread(testRespImgFile);
+testRespImgHeight = size(testRespImg,1);
+testRespImgWidth = size(testRespImg,2);
+testRespImg = Screen('MakeTexture',w,testRespImg);
 
 for b = 1:phaseCfg.nBlocks
-  % Run study task
-  recogphase = 'study';
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Run the recognition study task
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  recogphase = 'recog_study';
   
   % TODO: instructions
-  instructions = sprintf('Press ''%s'' to begin study.',KbName(cfg.keys.s00));
+  instructions = sprintf('Press ''%s'' to begin Recognition study task.',KbName(cfg.keys.s00));
   % put the instructions on the screen
   DrawFormattedText(w, instructions, 'center', 'center', WhiteIndex(w));
   % Update the display to show the instruction text:
   Screen('Flip', w);
   % wait until spacebar is pressed
-  RestrictKeysForKbCheck(cfg.keys.s00);
-  KbWait(-1);
+  RestrictKeysForKbCheck(KbName('space'));
+  KbWait(-1,2);
   RestrictKeysForKbCheck([]);
   % Clear screen to background color (our 'gray' as set at the
   % beginning):
@@ -92,45 +84,47 @@ for b = 1:phaseCfg.nBlocks
   % debug
   fprintf('study block %d\n',b);
   
-  % load up the stimuli for this block (TODO: should we load all blocks?)
-  blockStims = cell(1,length(expParam.session.(sesName).(phase).targStims{b}));
+  % load up the stimuli for this block
+  blockStims = nan(1,length(expParam.session.(sesName).(phase).targStims{b}));
   for i = 1:length(expParam.session.(sesName).(phase).targStims{b})
     % this image
-    stimFile = fullfile(cfg.files.stimDir,expParam.session.(sesName).(phase).targStims{b}(i).familyStr,expParam.session.(sesName).(phase).targStims{b}(i).fileName);
-    if exist(stimFile,'file')
-      stimimg = imread(stimFile);
-      blockStims{i} = Screen('MakeTexture',w,stimimg);
+    stimImgFile = fullfile(cfg.files.stimDir,expParam.session.(sesName).(phase).targStims{b}(i).familyStr,expParam.session.(sesName).(phase).targStims{b}(i).fileName);
+    if exist(stimImgFile,'file')
+      stimImg = imread(stimImgFile);
+      blockStims(i) = Screen('MakeTexture',w,stimImg);
+      % TODO: optimized?
+      %blockStims(i) = Screen('MakeTexture',w,stimimg,[],1);
     else
-      error('Study stimulus %s does not exist!',stimFile);
+      error('Study stimulus %s does not exist!',stimImgFile);
     end
   end
   
+  % get the width and height of the final stimulus image
+  stimImgHeight = size(stimImg,1);
+  stimImgWidth = size(stimImg,2);
+  % set the stimulus image rectangle
+  stimImgRect = [0 0 stimImgWidth stimImgHeight];
+  stimImgRect = CenterRect(stimImgRect,cfg.screen.wRect);
+  
+  % set the fixation size
+  Screen('TextSize', w, cfg.text.fixsize);
+  % draw fixation
+  DrawFormattedText(w,'+','center','center',WhiteIndex(w));
+  Screen('Flip',w);
+  
   for i = 1:length(blockStims)
-    % debug
-    fprintf('\tstim %d\n',i);
-    
     % ISI between trials
     WaitSecs(cfg.stim.(sesName).(phase).study_isi);
-    fprintf('wait\n');
     
-    % draw fixation
-    %Screen('TextSize', w, cfg.text.cfg.text.fixsize);
-    %fprintf('textsize\n');
-    DrawFormattedText(w,'+','center','center',WhiteIndex(w));
-    fprintf('draw +\n');
-    Screen('Flip',w);
-    fprintf('flip\n');
     % fixation on screen before starting trial
     WaitSecs(cfg.stim.(sesName).(phase).study_preTarg);
-    fprintf('wait\n');
     
     % draw the stimulus
-    Screen('DrawTexture', w, blockStims{i});
-    fprintf('draw stim\n');
+    Screen('DrawTexture', w, blockStims(i), [], stimImgRect);
     
     % Show stimulus on screen at next possible display refresh cycle,
     % and record stimulus onset time in 'startrt':
-    [VBLTimestamp, dispTime] = Screen('Flip', w);
+    [imgOnSCreen, dispTime] = Screen('Flip', w);
     
     % while loop to show stimulus until subjects response or until
     % "duration" seconds elapsed.
@@ -141,40 +135,41 @@ for b = 1:phaseCfg.nBlocks
     end
     
     % Write presentation to file:
-    fprintf(logFile,'%d %s %i %i %s %s %i %i %i\n',...
-      VBLTimestamp,...
+    fprintf(logFile,'%f %s %i %i %s %s %s %i %i\n',...
+      imgOnSCreen,...
       recogphase,...
       b,...
       i,...
       'RECOGSTUDY_TARG',...
-      expParam.session.(sesName).(phase).allStims{b}(i).familyStr,...
-      expParam.session.(sesName).(phase).allStims{b}(i).speciesStr,...
-      expParam.session.(sesName).(phase).allStims{b}(i).exemplarName,...
-      expParam.session.(sesName).(phase).allStims{b}(i).targ);
+      expParam.session.(sesName).(phase).targStims{b}(i).familyStr,...
+      expParam.session.(sesName).(phase).targStims{b}(i).speciesStr,...
+      expParam.session.(sesName).(phase).targStims{b}(i).exemplarName,...
+      expParam.session.(sesName).(phase).targStims{b}(i).targ);
     
-    % Clear screen to background color after fixed 'duration'
+    % Clear screen to background color after fixed 'duration' and draw fixation
+    DrawFormattedText(w,'+','center','center',WhiteIndex(w));
     Screen('Flip', w);
     
     % Flush out screens before next trial
-    Screen('Close', blockStims{i});
+    Screen('Close', blockStims(i));
     
   end % for stimuli
   
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Run the recognition test task
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
-  
-  
-  % Run test task
-  recogphase = 'test';
+  recogphase = 'recog_test';
   
   % TODO: instructions
-  instructions = sprintf('Press ''%s'' to begin test.',KbName(cfg.keys.s00));
+  instructions = sprintf('Press ''%s'' to begin Recognition test task.',KbName(cfg.keys.s00));
   % put the instructions on the screen
   DrawFormattedText(w, instructions, 'center', 'center', WhiteIndex(w));
   % Update the display to show the instruction text:
   Screen('Flip', w);
   % wait until spacebar is pressed
-  RestrictKeysForKbCheck(cfg.keys.s00);
-  KbWait(-1);
+  RestrictKeysForKbCheck(KbName('space'));
+  KbWait(-1,2);
   RestrictKeysForKbCheck([]);
   % Clear screen to background color (our 'gray' as set at the
   % beginning):
@@ -186,40 +181,58 @@ for b = 1:phaseCfg.nBlocks
   % debug
   fprintf('test block %d\n',b);
   
-  % load up the stimuli for this block (TODO: should we load all blocks?)
-  blockStims = cell(1,length(expParam.session.(sesName).(phase).allStims{b}));
+  % load up the stimuli for this block
+  blockStims = nan(1,length(expParam.session.(sesName).(phase).allStims{b}));
   for i = 1:length(expParam.session.(sesName).(phase).allStims{b})
     % this image
-    stimFile = fullfile(cfg.files.stimDir,expParam.session.(sesName).(phase).allStims{b}(i).familyStr,expParam.session.(sesName).(phase).allStims{b}(i).fileName);
-    if exist(stimFile,'file')
-      stimimg = imread(stimFile);
-      blockStims{i} = Screen('MakeTexture',w,stimimg);
+    stimImgFile = fullfile(cfg.files.stimDir,expParam.session.(sesName).(phase).allStims{b}(i).familyStr,expParam.session.(sesName).(phase).allStims{b}(i).fileName);
+    if exist(stimImgFile,'file')
+      stimImg = imread(stimImgFile);
+      blockStims(i) = Screen('MakeTexture',w,stimImg);
+      % TODO: optimized?
+      %blockStims(i) = Screen('MakeTexture',w,stimimg,[],1);
     else
-      error('Test stimulus %s does not exist!',stimFile);
+      error('Test stimulus %s does not exist!',stimImgFile);
     end
   end
+  
+  % get the width and height of the final stimulus image
+  stimImgHeight = size(stimImg,1);
+  stimImgWidth = size(stimImg,2);
+  % set the stimulus image rectangle
+  stimImgRect = [0 0 stimImgWidth stimImgHeight];
+  stimImgRect = CenterRect(stimImgRect,cfg.screen.wRect);
+  
+  % set the response key rectangle
+  respKeyImgRect = CenterRect([0 0 testRespImgWidth testRespImgHeight],stimImgRect);
+  respKeyImgRect = AdjoinRect(respKeyImgRect,stimImgRect,RectBottom);
+  
+  % only check these keys
+  RestrictKeysForKbCheck([cfg.keys.recogDefUn, cfg.keys.recogMayUn, cfg.keys.recogMayF, cfg.keys.recogDefF, cfg.keys.recogRecoll]);
+  
+  % set the fixation size
+  Screen('TextSize', w, cfg.text.fixsize);
+  % draw fixation
+  DrawFormattedText(w,'+','center','center',WhiteIndex(w));
+  Screen('Flip',w);
   
   for i = 1:length(blockStims)
     % ISI between trials
     WaitSecs(cfg.stim.(sesName).(phase).test_isi);
     
-    % draw fixation
-    Screen('TextSize', w, cfg.text.fixsize);
-    DrawFormattedText(w,'+','center','center',WhiteIndex(w));
-    Screen('Flip',w);
     % fixation on screen before starting trial
     WaitSecs(cfg.stim.(sesName).(phase).test_preStim);
     
     % draw the stimulus
-    Screen('DrawTexture', w, blockStims{i});
+    Screen('DrawTexture', w, blockStims(i), [], stimImgRect);
     
     % Show stimulus on screen at next possible display refresh cycle,
     % and record stimulus onset time in 'startRT':
-    [VBLTimestamp, startRT] = Screen('Flip', w);
+    [imgOnSCreen, startRT] = Screen('Flip', w);
     
     % Write presentation to file:
-    fprintf(logFile,'%d %s %i %i %s %s %s %i %i\n',...
-      VBLTimestamp,...
+    fprintf(logFile,'%f %s %i %i %s %s %s %i %i\n',...
+      imgOnSCreen,...
       recogphase,...
       b,...
       i,...
@@ -237,13 +250,12 @@ for b = 1:phaseCfg.nBlocks
       WaitSecs(0.001);
     end
     
-    % change + to ?
-    Screen('DrawTexture', w, blockStims{i});
-    DrawFormattedText(w,'?','center',(cfg.screen.xy(2)/2) + (cfg.screen.xy(2)*0.1),BlackIndex(w));
+    % draw the stimulus
+    Screen('DrawTexture', w, blockStims(i), [], stimImgRect);
+    % draw the response key image
+    Screen('DrawTexture', w, testRespImg, [], respKeyImgRect);
+    % put them on the screen
     Screen('Flip', w);
-    
-    % Flush out keyboard so they can't hold down a key
-    FlushEvents('keyDown');
     
     % initialize the keyboard check
     keyIsDown = 0;
@@ -252,7 +264,7 @@ for b = 1:phaseCfg.nBlocks
     while ~keyIsDown
       [keyIsDown, endRT, keyCode] = KbCheck;
       % if they push more than one key, don't accept it;
-      % TODO: this is not a good way to do this
+      % TODO: this doesn't seem like a good way to do this
       if sum(keyCode) == 1
         if keyCode(cfg.keys.recogDefUn) == 1 || keyCode(cfg.keys.recogMayUn) == 1 || keyCode(cfg.keys.recogMayF) == 1 || keyCode(cfg.keys.recogDefF) == 1 || keyCode(cfg.keys.recogRecoll) == 1
           break
@@ -265,28 +277,29 @@ for b = 1:phaseCfg.nBlocks
       end
       WaitSecs(0.001);
     end
-    %
-    %  break
-    %end
-    %[KeyIsDown, endRT, keyCode] = KbCheck;
     
-    % Clear screen to background color after fixed 'duration'
-    % or after subjects response (on test phase)
+    % Clear screen to background color after response and draw fixation
+    DrawFormattedText(w,'+','center','center',WhiteIndex(w));
     Screen('Flip', w);
     
+    % Flush out keyboard so they can't hold down a key (TODO: not working!)
+    FlushEvents('keyDown');
     
     % Close this stimulus before next trial
-    Screen('Close', blockStims{i});
+    Screen('Close', blockStims(i));
     
     % compute response time
     rt = round(1000 * (endRT - startRT));
     
     % compute accuracy
     if expParam.session.(sesName).(phase).allStims{b}(i).targ && (keyCode(cfg.keys.recogMayF) == 1 || keyCode(cfg.keys.recogDefF) == 1 || keyCode(cfg.keys.recogRecoll) == 1)
+      % target (hit)
       acc = 1;
     elseif ~expParam.session.(sesName).(phase).allStims{b}(i).targ && (keyCode(cfg.keys.recogDefUn) == 1 || keyCode(cfg.keys.recogMayUn) == 1)
+      % lure (correct rejection)
       acc = 1;
     else
+      % miss or false alarm
       acc = 0;
     end
     
@@ -294,7 +307,7 @@ for b = 1:phaseCfg.nBlocks
     resp = KbName(keyCode);
     
     % Write trial result to file:
-    fprintf(logFile,'%d %s %i %i %s %s %i %i %i %s %i %i\n',...
+    fprintf(logFile,'%f %s %i %i %s %s %i %i %i %s %i %i\n',...
       endRT,...
       recogphase,...
       b,...
@@ -310,9 +323,9 @@ for b = 1:phaseCfg.nBlocks
     
   end % for stimuli
   
+  % reset the KbCheck
+  RestrictKeysForKbCheck([]);
+  
 end % for nBlocks
-
-% reset the KbCheck
-RestrictKeysForKbCheck([]);
 
 end % function
