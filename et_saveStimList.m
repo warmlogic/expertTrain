5738d0a7c68da3429e50db206b6cfa8ede2cf160
Matt Mollison <matt.mollison@gmail.com>
2013-05-24 00:20:08 -0700

    arbitrary number of families, improved yoking

--- et_saveStimList.m
+++ et_saveStimList.m
@@ -11,148 +11,122 @@ if exist(cfg.files.expParamFile,'file')
   error('Experiment parameter file should not exist before stimulus list has been created: %s',cfg.files.expParamFile);
 end
 
-%%%%%%%%%%%%%
-% Family 1
-%%%%%%%%%%%%%
-
-fam1 = dir(fullfile(cfg.files.stimDir,cfg.stim.familyNames{1},['*',cfg.files.stimFileExt]));
-fam1 = {fam1.name};
-fam1Count = 0;
-fam1_tmp = cell(size(fam1));
-specStart = 1;
-% for saving the exemplar number
-fam1_eNum = zeros(size(fam1));
-% because there is no leading zero on the exemplar number, put the
-% files in the correct order (whether this is necessary depends on the
-% output of 'dir')
-
-% get all the species letters for this family
-specStr = cellfun(@(x) x(2), fam1, 'UniformOutput', false);
-% get the unique species letters for this family
-fam1_specStr = unique(specStr,'stable');
-
-for s = 1:length(fam1_specStr)
-  % slice out only this species
-  sInd = ismember(specStr,fam1_specStr(s));
-  % save the exemplar number
-  fam1_eNum(sInd) = 1:length(find(sInd));
-  fam1Species = fam1(sInd);
-  fam1Count = fam1Count + length(fam1Species);
-  % initialize
-  fam1Nums = nan(size(fam1Species));
-  % get the actual exemplar numbers for sorting
-  for i = 1:length(fam1Species)
-    speciesStr = fam1Species{i}(2);
-    % do strrep for the fileExt in case a letter matches
-    fam1Nums(i) = str2double(strrep(strrep(fam1Species{i}(2:end),speciesStr,''),strrep(cfg.files.stimFileExt,speciesStr,''),''));
-  end
-  [~,j] = sort(fam1Nums);
-  % put them in the "correct" order
-  fam1_tmp(specStart:fam1Count) = fam1Species(j);
-  specStart = specStart + length(fam1Species);
-  cfg.stim.nExemplars(1,s) = length(fam1Species);
-end
-filled_cells = cell2mat(cellfun(@(x) ~isempty(x), fam1_tmp, 'UniformOutput', false));
-fam1 = fam1_tmp(filled_cells);
-fam1_eNum = fam1_eNum(filled_cells);
-
-%%%%%%%%%%%%%
-% Family 2
-%%%%%%%%%%%%%
-
-fam2 = dir(fullfile(cfg.files.stimDir,cfg.stim.familyNames{2},['*',cfg.files.stimFileExt]));
-fam2 = {fam2.name};
-fam2Count = 0;
-fam2_tmp = cell(size(fam2));
-specStart = 1;
-% for saving the exemplar number
-fam2_eNum = zeros(size(fam1));
-% because there is no leading zero on the exemplar number, put the
-% files in the correct order (whether this is necessary depends on the
-% output of 'dir')
-
-% get all the species letters for this family
-specStr = cellfun(@(x) x(2), fam2, 'UniformOutput', false);
-% get the unique species letters for this family
-fam2_specStr = unique(specStr,'stable');
-
-for s = 1:length(fam2_specStr);
-  % slice out only this species
-  sInd = ismember(specStr,fam2_specStr(s));
-  % save the exemplar number
-  fam2_eNum(sInd) = 1:length(find(sInd));
-  fam2Species = fam2(sInd);
-  fam2Count = fam2Count + length(fam2Species);
-  % initialize
-  fam2Nums = nan(size(fam2Species));
-  % get the numbers for sorting
-  for i = 1:length(fam2Species)
-    speciesStr = fam2Species{i}(2);
-    % do strrep for the fileExt in case a letter matches
-    fam2Nums(i) = str2double(strrep(strrep(fam2Species{i}(2:end),speciesStr,''),strrep(cfg.files.stimFileExt,speciesStr,''),''));
-  end
-  [~,j] = sort(fam2Nums);
-  % put them in the "correct" order
-  fam2_tmp(specStart:fam2Count) = fam2Species(j);
-  specStart = specStart + length(fam2Species);
-  cfg.stim.nExemplars(2,s) = length(fam2Species);
-end
-filled_cells = cell2mat(cellfun(@(x) ~isempty(x), fam2_tmp, 'UniformOutput', false));
-fam2 = fam2_tmp(filled_cells);
-fam2_eNum = fam2_eNum(filled_cells);
-
-% randomize the species number so every subject gets a random species
-% assigned to each number
-if shuffleSpecies
-  fam1_specNum = randperm(length(fam1_specStr));
-  if cfg.stim.yokeSpeciesAcrossFamilies
-    if length(fam2_specStr) == length(fam1_specStr)
-      fam2_specNum = fam1_specNum;
-    else
-      warning('Different number of species in family 1 (%d) and 2 (%d), cannot yoke species numbers across families.',length(fam1_specStr),length(fam2_specStr));
-      cfg.stim.yokeSpeciesAcrossFamilies = false;
-      fam2_specNum = randperm(length(fam2_specStr));
-    end
-  else
-    fam2_specNum = randperm(length(fam2_specStr));
-  end
-else
-  fam1_specNum = 1:length(fam1_specStr);
-  fam2_specNum = 1:length(fam2_specStr);
-end
-% store the indices for later
-cfg.stim.specNum = nan(length(cfg.stim.familyNames),length(fam1_specStr));
-cfg.stim.specNum(1,:) = fam1_specNum;
-cfg.stim.specNum(2,:) = fam2_specNum;
-%cfg.stim.fam1_specNum = fam1_specNum;
-%cfg.stim.fam2_specNum = fam2_specNum;
-cfg.stim.specStr = cell(length(cfg.stim.familyNames),length(fam2_specStr));
-cfg.stim.specStr(1,:) = fam1_specStr;
-cfg.stim.specStr(2,:) = fam1_specStr;
-
 % for this subject, write out the file containing all stimuli
 fid = fopen(cfg.stim.file,'w');
 fprintf(fid,'fileName\tfamilyStr\tfamilyNum\tspeciesStr\tspeciesNum\texemplarName\texemplarNum\tstimNum\n');
 
-for i = 1:fam1Count
-  fStr = fam1{i}(1);
-  fNum = find(ismember(cfg.stim.familyNames,fStr));
-  sStr = fam1{i}(2);
-  sNum = cfg.stim.specNum(fNum,ismember(fam1_specStr,sStr));
-  [~,name] = fileparts(fam1{i});
-  eName = str2double(strrep(name,[fStr sStr],''));
-  fprintf(fid,'%s\t%s\t%d\t%s\t%d\t%d\t%d\t%d\n',fam1{i},fStr,fNum,sStr,sNum,eName,fam1_eNum(i),i);
-end
-
-for i = 1:fam2Count
-  fStr = fam2{i}(1);
-  fNum = find(ismember(cfg.stim.familyNames,fStr));
-  sStr = fam2{i}(2);
-  sNum = cfg.stim.specNum(fNum,ismember(fam2_specStr,sStr));
-  [~,name] = fileparts(fam2{i});
-  eName = str2double(strrep(name,[fStr sStr],''));
-  fprintf(fid,'%s\t%s\t%d\t%s\t%d\t%d\t%d\t%d\n',fam2{i},fStr,fNum,sStr,sNum,eName,fam2_eNum(i),i);
-end
+for f = 1:length(cfg.stim.familyNames)
+  fam = dir(fullfile(cfg.files.stimDir,cfg.stim.familyNames{f},['*',cfg.files.stimFileExt]));
+  fam = {fam.name};
+  % remove the file extension
+  fam = cellfun(@(x) strrep(x,cfg.files.stimFileExt,''), fam, 'UniformOutput', false);
+  
+  % initialize some counters
+  famCount = 0;
+  specStart = 1;
+  % for saving the full stimulus names in sorted order
+  fam_tmp = cell(size(fam));
+  % for saving the species string in sorted order
+  specStr_tmp = cell(size(fam));
+  % for saving the exemplar numbers from fam names in sorted order
+  exempNums_tmp = nan(size(fam));
+  % for saving the exemplar number
+  fam_eNum = zeros(size(fam));
+  
+  % because there is no leading zero on the exemplar number, put the
+  % files in the correct order (whether this is necessary depends on the
+  % output of 'dir', which depends on the operating system)
+  
+  % get all the species letters for this family
+  specExempStr = cell(size(fam));
+  for i = 1:length(fam)
+    % find where in the string the family name occurs
+    fnInd = strfind(fam{i},cfg.stim.familyNames{f});
+    % only remove the first instance
+    specExempStr{i} = fam{i}((fnInd + length(cfg.stim.familyNames{f})):end);
+  end
+  % remove the exemplar numbers (only keep the characters that are in the
+  % alphabet letter range)
+  specStr = cellfun(@(x) x(isstrprop(x,'alpha')), specExempStr, 'UniformOutput', false);
+  % get the actual exemplar numbers of this species for sorting
+  exempNums = str2double(cellfun(@(x) x(isstrprop(x,'digit')), specExempStr, 'UniformOutput', false));
+  
+  % get the unique species letters for this family
+  if ~exist('uniqueSpecStr','var')
+    uniqueSpecStr = unique(specStr,'stable');
+  else
+    uniqueSpecStr = cat(1,uniqueSpecStr,unique(specStr,'stable'));
+  end
+  
+  % put this species in the correct order
+  for s = 1:length(uniqueSpecStr(f,:))
+    % slice out only this species
+    sInd = find(ismember(specStr,uniqueSpecStr(f,s)));
+    % save the actual exemplar count
+    fam_eNum(sInd) = 1:length(sInd);
+    famCount = famCount + length(sInd);
+    
+    % sort the exemplar numbers for this species
+    [~,j] = sort(exempNums(sInd));
+    % put them in the "correct" order
+    fam_tmp(specStart:famCount) = fam(sInd(j));
+    specStr_tmp(specStart:famCount) = specStr(sInd(j));
+    exempNums_tmp(specStart:famCount) = exempNums(sInd(j));
+    specStart = specStart + length(sInd);
+    cfg.stim.nExemplars(f,s) = length(sInd);
+  end
+  % make sure we're only grabbing real entires
+  filled_cells = cell2mat(cellfun(@(x) ~isempty(x), fam_tmp, 'UniformOutput', false));
+  fam = fam_tmp(filled_cells);
+  specStr = specStr_tmp(filled_cells);
+  exempNums = exempNums_tmp(filled_cells);
+  fam_eNum = fam_eNum(filled_cells);
+  
+  if shuffleSpecies
+    if ~exist('specNum','var')
+      specNum = randperm(length(uniqueSpecStr(f,:)));
+    else
+      if cfg.stim.yokeSpeciesAcrossFamilies
+        % assumes that all families have the same number of species
+        if length(uniqueSpecStr(f,:)) == length(uniqueSpecStr(f-1,:))
+          if cfg.stim.yokeTogether(f) == cfg.stim.yokeTogether(f-1)
+            specNum = cat(1,specNum,specNum(f-1,:));
+          else
+            specNum = cat(1,specNum,randperm(length(uniqueSpecStr(f,:))));
+          end
+        else
+          error('There are different numbers of species in family %d (%d) and %d (%d), so it is not possible to yoke species numbers across families.',f-1,length(uniqueSpecStr(f-1,:)),f,length(uniqueSpecStr(f,:)));
+        end
+      else
+        specNum = cat(1,specNum,randperm(length(uniqueSpecStr(f,:))));
+      end
+    end
+  else
+    if ~exist('specNum','var')
+      specNum = 1:length(uniqueSpecStr(f,:));
+    else
+      specNum = cat(1,specNum,1:length(uniqueSpecStr(f,:)));
+    end
+  end
+  
+  % store the indices for later
+  if ~isfield(cfg.stim,'specNum') || isempty(cfg.stim.specNum)
+    cfg.stim.specNum = specNum(f,:);
+  else
+    cfg.stim.specNum = cat(1,cfg.stim.specNum,specNum(f,:));
+  end
+  
+  % print the stimulus info to the stimulus list file
+  for i = 1:famCount
+    %sStr = specStr{i};
+    sNum = cfg.stim.specNum(f,ismember(uniqueSpecStr(f,:),specStr{i}));
+    fprintf(fid,'%s%s\t%s\t%d\t%s\t%d\t%d\t%d\t%d\n',...
+      fam{i},cfg.files.stimFileExt,...
+      cfg.stim.familyNames{f},f,...
+      specStr{i},sNum,...
+      exempNums(i),fam_eNum(i),i);
+  end
+end % for each family
 
 fclose(fid);
 
