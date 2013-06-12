bd6e2b5d2ecba8f89a65043360f81134edf0ef9c
Matt Mollison <matt.mollison@gmail.com>
2013-06-12 11:17:22 -0600

    cleanup

--- config_EBIRD.m
+++ config_EBIRD.m
@@ -328,7 +328,6 @@ if expParam.sessionNum == 1
   %     cfg.files.recogTestRespKeyImg = fullfile(cfg.files.resDir,sprintf('recogTest_resp_black_middle_%d.jpg',cfg.keys.recogKeySet));
   %     %cfg.files.recogTestRespKeyImg = fullfile(cfg.files.resDir,sprintf('recogTest_resp_white_middle_%d.jpg',cfg.keys.recogKeySet));
   %   end
-  %   %cfg.files.recogTestRespKeyImg = fullfile(cfg.files.resDir,sprintf('recog_test_resp%d.jpg',cfg.keys.recogKeySet));
   %
   %   % scale image down (< 1) or up (> 1)
   %   cfg.files.recogTestRespKeyImgScale = 0.6;
