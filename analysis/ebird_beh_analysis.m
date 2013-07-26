05a34edc2663059b437a226c004ba7da33b71302
Matt Mollison <matt.mollison@gmail.com>
2013-07-25 22:29:42 -0600

    streamlining

--- analysis/ebird_beh_analysis.m
+++ analysis/ebird_beh_analysis.m
@@ -25,6 +25,8 @@ if nargin == 0
     };
 end
 
+trainedConds = {0, 1, [0 1]};
+
 for sub = 1:length(subjects)
   subDir = fullfile(dataroot,subjects{sub});
   fprintf('Processing %s in %s...\n',subjects{sub},subDir);
@@ -78,76 +80,77 @@ for sub = 1:length(subjects)
             switch phaseName
               case {'match', 'prac_match'}
                 
-                % filter the events that we want
-                matchResp = events.(sesName).(fn)(strcmp({events.(sesName).(fn).type},'MATCH_RESP'));
-                
-                % exclude missed responses ('none')
-                matchResp = matchResp(~strcmp({matchResp.resp},'none'));
-                % % set missing responses to incorrect
-                % matchResp(strcmp({matchResp.resp},'none')).resp = 0;
-                
-                fprintf('%s, %s, %s\n',expParam.subject,sesName,fn);
-                
-                % compute overall accuracy
-                if length(matchResp) == length([matchResp.acc])
-                  matchAcc = mean([matchResp.acc]);
-                else
-                  fprintf('Something is wrong with matching trial counts\n');
-                  keyboard
-                end
-                fprintf('\tAccuracy:\t%.3f (%d/%d)\n',matchAcc,sum([matchResp.acc]),length([matchResp.acc]));
-                % compute overall RT
-                matchRT = mean([matchResp.rt]);
-                matchRTcor = mean([matchResp([matchResp.acc] == 1).rt]);
-                matchRTinc = mean([matchResp([matchResp.acc] == 0).rt]);
-                fprintf('\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchRT,matchRTcor,matchRTinc);
-                
-                % basic and subordinate accuracy
-                matchBasic = matchResp([matchResp.isSubord] == 0);
-                matchSubord = matchResp([matchResp.isSubord] == 1);
-                matchBasicAcc = mean([matchBasic.acc]);
-                matchSubordAcc = mean([matchSubord.acc]);
-                fprintf('\t\tBasic accuracy:\t\t%.3f (%d/%d)\n',matchBasicAcc,sum([matchBasic.acc]),length([matchBasic.acc]));
-                fprintf('\t\tSubordinate accuracy:\t%.3f (%d/%d)\n',matchSubordAcc,sum([matchSubord.acc]),length([matchSubord.acc]));
-                % basic and subordinate RT
-                matchBasicRT = mean([matchBasic.rt]);
-                matchBasicRTcor = mean([matchBasic([matchBasic.acc] == 1).rt]);
-                matchBasicRTinc = mean([matchBasic([matchBasic.acc] == 0).rt]);
-                matchSubordRT = mean([matchSubord.rt]);
-                matchSubordRTcor = mean([matchSubord([matchSubord.acc] == 1).rt]);
-                matchSubordRTinc = mean([matchSubord([matchSubord.acc] == 0).rt]);
-                fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchBasicRT,matchBasicRTcor,matchBasicRTinc);
-                fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchSubordRT,matchSubordRTcor,matchSubordRTinc);
-                
-                % accuracy for the different image manipulation conditions
-                imgConds = unique({matchResp.imgCond});
-                if length(imgConds) > 1
-                  fprintf('\n');
-                  for im = 1:length(imgConds)
-                    matchCond = matchResp(strcmp({matchResp.imgCond},imgConds{im}));
-                    matchCondAcc = mean([matchCond.acc]);
-                    fprintf('\t%s:\tAccuracy:\t%.3f (%d/%d)\n',imgConds{im},matchCondAcc,sum([matchCond.acc]),length([matchCond.acc]));
-                    
-                    % basic and subordinate for this manipulation
-                    matchCondBasic = matchResp([matchCond.isSubord] == 0);
-                    matchCondSubord = matchResp([matchCond.isSubord] == 1);
-                    matchCondBasicAcc = mean([matchCondBasic.acc]);
-                    matchCondSubordAcc = mean([matchCondSubord.acc]);
-                    fprintf('\t\tBasic accuracy:\t\t%.3f (%d/%d)\n',matchCondBasicAcc,sum([matchCondBasic.acc]),length([matchCondBasic.acc]));
-                    fprintf('\t\tSubord accuracy:\t%.3f (%d/%d)\n',matchCondSubordAcc,sum([matchCondSubord.acc]),length([matchCondSubord.acc]));
-                    % response time
-                    matchCondBasicRT = mean([matchCondBasic.rt]);
-                    matchCondBasicRTcor = mean([matchCondBasic([matchCondBasic.acc] == 1).rt]);
-                    matchCondBasicRTinc = mean([matchCondBasic([matchCondBasic.acc] == 0).rt]);
-                    matchCondSubordRT = mean([matchCondSubord.rt]);
-                    matchCondSubordRTcor = mean([matchCondSubord([matchCondSubord.acc] == 1).rt]);
-                    matchCondSubordRTinc = mean([matchCondSubord([matchCondSubord.acc] == 0).rt]);
-                    fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchCondBasicRT,matchCondBasicRTcor,matchCondBasicRTinc);
-                    fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchCondSubordRT,matchCondSubordRTcor,matchCondSubordRTinc);
+                for t = 1:length(trainedConds)
+                  
+                  fprintf('%s, %s, %s\n',expParam.subject,sesName,fn);
+                  
+                  if length(trainedConds{t}) == 1
+                    if trainedConds{t} == 1
+                      fprintf('Trained\n');
+                      trainStr = 'trained';
+                    elseif trainedConds{t} == 0
+                      fprintf('Untrained\n');
+                      trainStr = 'untrained';
+                    end
+                  elseif length(trainedConds{t}) > 1
+                    fprintf('Trained and untrained together\n');
+                    trainStr = 'all';
                   end
+                  
+                  % filter the events that we want
+                  matchResp = events.(sesName).(fn)(strcmp({events.(sesName).(fn).type},'MATCH_RESP') & ismember([events.(sesName).(fn).trained],trainedConds{t}));
+                  
+                  % % exclude missed responses ('none')
+                  % matchResp = matchResp(~strcmp({matchResp.resp},'none'));
+                  % % set missing responses to incorrect
+                  % noRespInd = find(strcmp({matchResp.resp},'none'));
+                  % if ~isempty(noRespInd)
+                  %   for nr = 1:length(noRespInd)
+                  %     matchResp(noRespInd(nr)).acc = 0;
+                  %   end
+                  % end
+                  
+                  matchResults.(trainStr) = accAndRT(matchResp);
+                  fprintf('\tAccuracy:\t%.4f (%d/%d)\n',matchResults.(trainStr).acc,sum([matchResp.acc] == 1),length([matchResp.acc]));
+                  fprintf('\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchResults.(trainStr).rt,matchResults.(trainStr).rt_cor,matchResults.(trainStr).rt_inc);
+                  
+                  % basic and subordinate accuracy
+                  matchBasic = matchResp([matchResp.isSubord] == 0);
+                  matchSubord = matchResp([matchResp.isSubord] == 1);
+                  
+                  matchBasicResults.(trainStr) = accAndRT(matchBasic);
+                  matchSubordResults.(trainStr) = accAndRT(matchSubord);
+                  fprintf('\t\tBasic acc:\t%.4f (%d/%d)\n',matchBasicResults.(trainStr).acc,sum([matchBasic.acc] == 1),length([matchBasic.acc]));
+                  fprintf('\t\tSubord acc:\t%.4f (%d/%d)\n',matchSubordResults.(trainStr).acc,sum([matchSubord.acc] == 1),length([matchSubord.acc]));
+                  fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchBasicResults.(trainStr).rt,matchBasicResults.(trainStr).rt_cor,matchBasicResults.(trainStr).rt_inc);
+                  fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchSubordResults.(trainStr).rt,matchSubordResults.(trainStr).rt_cor,matchSubordResults.(trainStr).rt_inc);
+                  
+                  % accuracy for the different image manipulation conditions
+                  imgConds = unique({matchResp.imgCond});
+                  if length(imgConds) > 1
+                    fprintf('\n');
+                    for im = 1:length(imgConds)
+                      matchCond = matchResp(strcmp({matchResp.imgCond},imgConds{im}));
+                      matchCondResults.(trainStr) = accAndRT(matchCond);
+                      fprintf('\t%s:\tAccuracy:\t%.4f (%d/%d)\n',imgConds{im},matchCondResults.(trainStr).acc,sum([matchCond.acc] == 1),length([matchCond.acc]));
+                      fprintf('\t%s:\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',imgConds{im},matchCondResults.(trainStr).rt,matchCondResults.(trainStr).rt_cor,matchCondResults.(trainStr).rt_inc);
+                      
+                      % basic and subordinate for this manipulation
+                      matchCondBasic = matchResp([matchCond.isSubord] == 0);
+                      matchCondSubord = matchResp([matchCond.isSubord] == 1);
+                      matchCondBasicResults.(trainStr) = accAndRT(matchCondBasic);
+                      matchCondSubordResults.(trainStr) = accAndRT(matchCondSubord);
+                      fprintf('\t\t\tBasic acc:\t%.4f (%d/%d)\n',matchCondBasicResults.(trainStr).acc,sum([matchCondBasic.acc] == 1),length([matchCondBasic.acc]));
+                      fprintf('\t\t\tSubord acc:\t%.4f (%d/%d)\n',matchCondSubordResults.(trainStr).acc,sum([matchCondSubord.acc] == 1),length([matchCondSubord.acc]));
+                      fprintf('\t\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchCondBasicResults.(trainStr).rt,matchCondBasicResults.(trainStr).rt_cor,matchCondBasicResults.(trainStr).rt_inc);
+                      fprintf('\t\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',matchCondSubordResults.(trainStr).rt,matchCondSubordResults.(trainStr).rt_cor,matchCondSubordResults.(trainStr).rt_inc);
+                    end
+                  end
+                  
                 end
                 
               case {'name', 'nametrain', 'prac_name'}
+                fprintf('%s, %s, %s\n',expParam.subject,sesName,fn);
                 
                 if ~iscell(expParam.session.(sesName).(phaseName)(phaseCount).nameStims)
                   nBlocks = 1;
@@ -158,91 +161,53 @@ for sub = 1:length(subjects)
                 % filter the events that we want
                 nameResp = events.(sesName).(fn)(strcmp({events.(sesName).(fn).type},'NAME_RESP'));
                 
-                % exclude missed responses ('none')
-                nameResp = nameResp([nameResp.resp] ~= -1);
-                % % set missing response to incorrect
-                % nameResp([nameResp.resp] == -1) = 0;
+                % % exclude missed responses (-1)
+                % nameResp = nameResp([nameResp.resp] ~= -1);
+                % set missing response to incorrect
+                % noRespInd = find([nameResp.resp] == -1);
+                % if ~isempty(noRespInd)
+                %   for nr = 1:length(noRespInd)
+                %     nameResp(noRespInd(nr)).acc = 0;
+                %   end
+                % end
                 
-                if length(nameResp) == length([nameResp.acc])
-                  nameAcc = mean([nameResp.acc]);
-                else
-                  fprintf('Something is wrong with naming trial counts\n');
-                  keyboard
-                end
-                
-                fprintf('%s, %s, %s\n',expParam.subject,sesName,fn);
-                % compute overall accuracy
-                if nBlocks == 1
-                  descripStr = 'Accuracy';
-                elseif nBlocks > 1
-                  descripStr = 'Overall accuracy';
-                end
-                fprintf('\t%s:\t%.3f (%d/%d)\n',descripStr,nameAcc,sum([nameResp.acc]),length([nameResp.acc]));
-                % compute overall RT
-                nameRT = mean([nameResp.rt]);
-                nameRTcor = mean([nameResp([nameResp.acc] == 1).rt]);
-                nameRTinc = mean([nameResp([nameResp.acc] == 0).rt]);
-                fprintf('\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameRT,nameRTcor,nameRTinc);
+                nameResults = accAndRT(nameResp);
+                fprintf('\tAccuracy:\t%.4f (%d/%d)\n',nameResults.acc,sum([nameResp.acc] == 1),length([nameResp.acc]));
+                fprintf('\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameResults.rt,nameResults.rt_cor,nameResults.rt_inc);
                 
                 % basic and subordinate accuracy
                 nameBasic = nameResp([nameResp.isSubord] == 0);
                 nameSubord = nameResp([nameResp.isSubord] == 1);
-                nameBasicAcc = mean([nameBasic.acc]);
-                nameSubordAcc = mean([nameSubord.acc]);
-                fprintf('\t\tBasic accuracy:\t\t%.3f (%d/%d)\n',nameBasicAcc,sum([nameBasic.acc]),length([nameBasic.acc]));
-                fprintf('\t\tSubord accuracy:\t%.3f (%d/%d)\n',nameSubordAcc,sum([nameSubord.acc]),length([nameSubord.acc]));
-                % basic and subordinate RT
-                nameBasicRT = mean([nameBasic.rt]);
-                nameBasicRTcor = mean([nameBasic([nameBasic.acc] == 1).rt]);
-                nameBasicRTinc = mean([nameBasic([nameBasic.acc] == 0).rt]);
-                nameSubordRT = mean([nameSubord.rt]);
-                nameSubordRTcor = mean([nameSubord([nameSubord.acc] == 1).rt]);
-                nameSubordRTinc = mean([nameSubord([nameSubord.acc] == 0).rt]);
-                fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBasicRT,nameBasicRTcor,nameBasicRTinc);
-                fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameSubordRT,nameSubordRTcor,nameSubordRTinc);
                 
-                % % accuracy for the different image manipulation conditions
-                % imgConds = unique({nameResp.imgCond});
-                % if length(imgConds) > 1
-                %   for im = 1:length(imgConds)
-                %     nameCond = nameResp(strcmp({nameResp.imgCond},imgConds{im}));
-                %     nameCondAcc = mean([nameCond.acc]);
-                %     fprintf('\t%s:\tAccuracy:\t%.3f (%d/%d)\n',imgConds{im},nameCondAcc,sum([nameCond.acc]),length([nameCond.acc]));
-                %   end
-                % end
+                nameBasicResults = accAndRT(nameBasic);
+                nameSubordResults = accAndRT(nameSubord);
+                fprintf('\t\tBasic acc:\t%.4f (%d/%d)\n',nameBasicResults.acc,sum([nameBasic.acc] == 1),length([nameBasic.acc]));
+                fprintf('\t\tSubord acc:\t%.4f (%d/%d)\n',nameSubordResults.acc,sum([nameSubord.acc] == 1),length([nameSubord.acc]));
+                fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBasicResults.rt,nameBasicResults.rt_cor,nameBasicResults.rt_inc);
+                fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameSubordResults.rt,nameSubordResults.rt_cor,nameSubordResults.rt_inc);
                 
                 if nBlocks > 1
                   fprintf('\n');
                   for b = 1:nBlocks
+                    fprintf('Block %d\n',b);
+                    
+                    blockStr = sprintf('b%d',b);
                     nameBlock = nameResp([nameResp.block] == b);
                     
-                    if length(nameBlock) == length([nameBlock.acc])
-                      nameBlockAcc = mean([nameBlock.acc]);
-                    else
-                      fprintf('Something is wrong with naming trial counts\n');
-                      keyboard
-                    end
+                    nameBlockResults.(blockStr) = accAndRT(nameBlock);
+                    fprintf('\t%s:\tAccuracy:\t%.4f (%d/%d)\n',imgConds{im},nameBlockResults.(blockStr).acc,sum([nameBlock.acc] == 1),length([nameBlock.acc]));
+                    fprintf('\t%s:\tRespTime:\t%.2f ms (cor: %.2f, inc: %.2f)\n',imgConds{im},nameBlockResults.(blockStr).rt,nameBlockResults.(blockStr).rt_cor,nameBlockResults.(blockStr).rt_inc);
                     
-                    fprintf('\tBlock %d: Accuracy:\t%.3f (%d/%d)\n',b,nameBlockAcc,sum([nameBlock.acc]),length([nameBlock.acc]));
+                    % basic and subordinate for this manipulation
+                    nameBlockBasic = matchResp([nameBlock.isSubord] == 0);
+                    nameBlockSubord = matchResp([nameBlock.isSubord] == 1);
+                    nameBlockBasicResults.(blockStr) = accAndRT(nameBlockBasic);
+                    nameBlockSubordResults.(blockStr) = accAndRT(nameBlockSubord);
+                    fprintf('\t\t\tBasic acc:\t%.4f (%d/%d)\n',nameBlockBasicResults.(blockStr).acc,sum([nameBlockBasic.acc] == 1),length([nameBlockBasic.acc]));
+                    fprintf('\t\t\tSubord acc:\t%.4f (%d/%d)\n',nameBlockSubordResults.(blockStr).acc,sum([nameBlockSubord.acc] == 1),length([nameBlockSubord.acc]));
+                    fprintf('\t\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBlockBasicResults.(blockStr).rt,nameBlockBasicResults.(blockStr).rt_cor,nameBlockBasicResults.(blockStr).rt_inc);
+                    fprintf('\t\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBlockSubordResults.(blockStr).rt,nameBlockSubordResults.(blockStr).rt_cor,nameBlockSubordResults.(blockStr).rt_inc);
                     
-                    % basic and subordinate within this block
-                    %
-                    % accuracy
-                    nameBlockBasic = nameResp([nameBlock.isSubord] == 0);
-                    nameBlockSubord = nameResp([nameBlock.isSubord] == 1);
-                    nameBlockBasicAcc = mean([nameBlockBasic.acc]);
-                    nameBlockSubordAcc = mean([nameBlockSubord.acc]);
-                    fprintf('\t\tBasic accuracy:\t\t%.3f (%d/%d)\n',nameBlockBasicAcc,sum([nameBlockBasic.acc]),length([nameBlockBasic.acc]));
-                    fprintf('\t\tSubord accuracy:\t%.3f (%d/%d)\n',nameBlockSubordAcc,sum([nameBlockSubord.acc]),length([nameBlockSubord.acc]));
-                    % response time
-                    nameBlockBasicRT = mean([nameBlockBasic.rt]);
-                    nameBlockBasicRTcor = mean([nameBlockBasic([nameBlockBasic.acc] == 1).rt]);
-                    nameBlockBasicRTinc = mean([nameBlockBasic([nameBlockBasic.acc] == 0).rt]);
-                    nameBlockSubordRT = mean([nameBlockSubord.rt]);
-                    nameBlockSubordRTcor = mean([nameBlockSubord([nameBlockSubord.acc] == 1).rt]);
-                    nameBlockSubordRTinc = mean([nameBlockSubord([nameBlockSubord.acc] == 0).rt]);
-                    fprintf('\t\tBasic RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBlockBasicRT,nameBlockBasicRTcor,nameBlockBasicRTinc);
-                    fprintf('\t\tSubord RT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',nameBlockSubordRT,nameBlockSubordRTcor,nameBlockSubordRTinc);
                   end
                 end
                 
@@ -268,3 +233,19 @@ end % for sub
 fprintf('\n');
 
 end % function
+
+%% Calculate accuracy and reaction time
+
+function results = accAndRT(thisCond)
+
+% accuracy
+results.acc = mean([thisCond.acc] == 1);
+%fprintf('\t\tAccuracy:\t\t%.4f (%d/%d)\n',thisCondAcc,sum([thisCond.acc]),length([thisCond.acc]));
+% RT
+results.rt = mean([thisCond.rt]);
+results.rt_cor = mean([thisCond([thisCond.acc] == 1).rt]);
+results.rt_inc = mean([thisCond([thisCond.acc] == 0).rt]);
+%fprintf('\t\tRT:\t%.2f ms (cor: %.2f, inc: %.2f)\n',thisCondRT,thisCondRTcor,thisCondRTinc);
+
+end
+
