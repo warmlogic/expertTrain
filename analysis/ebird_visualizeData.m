14e64429c0392919483ff86464248e03f9f6e32c
Matt Mollison <matt.mollison@gmail.com>
2013-07-26 17:12:20 -0600

    script for visualizing data in plots and graphs

---
+++ analysis/ebird_visualizeData.m
@@ -0,0 +1,238 @@
+% basic analysis script for expertTrain experiments
+
+% TODO: plot individual subjects along with mean/SEM
+
+% TODO: is there a better kind of plot to use, e.g., box and whisker?
+
+%% set the subjects and run the data processing script
+subjects = {
+  'EBIRD049';
+  'EBIRD002';
+  'EBIRD003';
+  };
+
+[results] = ebird_beh_analysis(subjects);
+
+%% plot basic and subordinate RTs across training days
+
+nTrainSes = 6;
+phases = {'name_1','name_2','name_3','name_4'};
+
+rt.overall = nan(length(subjects),nTrainSes,length(phases));
+rt.basic = nan(length(subjects),nTrainSes,length(phases));
+rt.subord = nan(length(subjects),nTrainSes,length(phases));
+
+% dataField = 'rt';
+dataField = 'rt_cor';
+
+for t = 1:nTrainSes
+  sesName = sprintf('train%d',t);
+  for p = 1:length(phases)
+    if isfield(results.(sesName),phases{p})
+      rt.overall(:,t,p) = results.(sesName).(phases{p}).overall.(dataField);
+      rt.basic(:,t,p) = results.(sesName).(phases{p}).basic.(dataField);
+      rt.subord(:,t,p) = results.(sesName).(phases{p}).subord.(dataField);
+    end
+  end
+end
+
+for p = 1:length(phases)
+  figure
+  
+  bRT_mean = nanmean(rt.basic(:,:,p),1);
+  bRT_sem = nanstd(rt.basic(:,:,p),1) ./ sqrt(sum(~isnan(rt.basic(:,:,p))));
+  %plot(1:nTrainDays,bRT_mean,'d-','LineWidth',2,'Color',[0.5 0.5 0.5]);
+  errorbar(bRT_mean,bRT_sem,'d-','LineWidth',2,'Color',[0.5 0.5 0.5]);
+  hold on
+  
+  sRT_mean = nanmean(rt.subord(:,:,p),1);
+  sRT_sem = nanstd(rt.subord(:,:,p),1) ./ sqrt(sum(~isnan(rt.subord(:,:,p))));
+  %plot(1:nTrainDays,sRT_mean,'ks-','LineWidth',2);
+  errorbar(sRT_mean,sRT_sem,'ks-','LineWidth',2);
+  
+  % oRT_mean = nanmean(rt.overall(:,:,p),1);
+  % oRT_sem = nanstd(rt.overall(:,:,p),1) ./ sqrt(sum(~isnan(rt.overall(:,:,p))));
+  % % plot(1:nTrainDays,oRT_mean,'ro-','LineWidth',2);
+  % errorbar(oRT_mean,oRT_sem,'ro-','LineWidth',2);
+  hold off
+  
+  %axis([0.5 6.5 0 round(max(rt.overall(:))/100)*100]);
+  axis([0.5 6.5 0 600]);
+  title(sprintf('Naming phase %d: %s',p,strrep(dataField,'_','\_')));
+  xlabel('Training Day');
+  ylabel('Response Time (ms)');
+  
+  legend({'Basic','Subordinate'},'Location','NorthEast');
+  %legend({'Basic','Subordinate','Overall'});
+  
+  publishfig(gcf,0);
+end
+
+%% plot basic and subordinate accuracy across training days
+
+nTrainSes = 6;
+phases = {'name_1','name_2','name_3','name_4'};
+
+acc.overall = nan(length(subjects),nTrainSes,length(phases));
+acc.basic = nan(length(subjects),nTrainSes,length(phases));
+acc.subord = nan(length(subjects),nTrainSes,length(phases));
+
+dataField = 'acc';
+
+for t = 1:nTrainSes
+  sesName = sprintf('train%d',t);
+  for p = 1:length(phases)
+    if isfield(results.(sesName),phases{p})
+      acc.overall(:,t,p) = results.(sesName).(phases{p}).overall.(dataField);
+      acc.basic(:,t,p) = results.(sesName).(phases{p}).basic.(dataField);
+      acc.subord(:,t,p) = results.(sesName).(phases{p}).subord.(dataField);
+    end
+  end
+end
+
+for p = 1:length(phases)
+  figure
+  
+  bAcc_mean = nanmean(acc.basic(:,:,p),1);
+  bAcc_sem = nanstd(acc.basic(:,:,p),1) ./ sqrt(sum(~isnan(acc.basic(:,:,p))));
+  %plot(1:nTrainDays,bAcc_mean,'d-','LineWidth',2,'Color',[0.5 0.5 0.5]);
+  errorbar(bAcc_mean,bAcc_sem,'d-','LineWidth',2,'Color',[0.5 0.5 0.5]);
+  hold on
+  
+  sAcc_mean = nanmean(acc.subord(:,:,p),1);
+  sAcc_sem = nanstd(acc.subord(:,:,p),1) ./ sqrt(sum(~isnan(acc.subord(:,:,p))));
+  %plot(1:nTrainDays,sAcc_mean,'ks-','LineWidth',2);
+  errorbar(sAcc_mean,sAcc_sem,'ks-','LineWidth',2);
+  
+  % oAcc_mean = nanmean(acc.overall(:,:,p),1);
+  % oAcc_sem = nanstd(acc.overall(:,:,p),1) ./ sqrt(sum(~isnan(acc.overall(:,:,p))));
+  % % plot(1:nTrainDays,oAcc_mean,'ro-','LineWidth',2);
+  % errorbar(oAcc_mean,oAcc_sem,'ro-','LineWidth',2);
+  hold off
+  
+  %axis([0.5 6.5 0 round(max(acc.overall(:))/100)*100]);
+  axis([0.5 6.5 0.5 1]);
+  title(sprintf('Naming phase %d: %s',p,strrep(dataField,'_','\_')));
+  xlabel('Training Day');
+  ylabel('Accuracy');
+  
+  legend({'Basic','Subordinate'},'Location','SouthEast');
+  %legend({'Basic','Subordinate','Overall'});
+  
+  publishfig(gcf,0);
+end
+
+%% plot basic and subordinate d' for pretest, poststest, posttest_delayed
+
+sessions = {'pretest', 'posttest', 'posttest_delay'};
+phases = {'match_1'};
+training = {'trained','untrained'};
+naming = {'basic','subord'};
+
+dataField = 'dp';
+
+dp = struct;
+for n = 1:length(naming)
+  dp.(naming{n}) = nan(length(subjects),length(sessions),length(phases),length(training));
+end
+
+for s = 1:length(sessions)
+  sesName = sessions{s};
+  for p = 1:length(phases)
+    for t = 1:length(training)
+      for n = 1:length(naming)
+        dp.(naming{n})(:,s,p,t) = results.(sesName).(phases{p}).(training{t}).(naming{n}).(dataField);
+      end
+    end
+  end
+end
+
+for p = 1:length(phases)
+  for n = 1:length(naming)
+    figure
+    bdp_mean = nan(length(sessions),length(training));
+    bdp_sem = nan(length(sessions),length(training));
+    
+    for t = 1:length(training)
+      for s = 1:length(sessions)
+        bdp_mean(s,t) = nanmean(dp.(naming{n})(:,s,p,t),1);
+        bdp_sem(s,t) = nanstd(dp.(naming{n})(:,s,p,t),1) ./ sqrt(sum(~isnan(dp.(naming{n})(:,s,p,t))));
+      end
+    end
+    
+    bw_title = sprintf('%s%s: %s',upper(naming{n}(1)),naming{n}(2:end),strrep(phases{p},'_','\_'));
+    bw_groupnames = {'Pretest', 'Posttest', 'One week later'};
+    bw_legend = {'Trained','Untrained'};
+    %bw_xlabel = 'Test day';
+    bw_xlabel = [];
+    bw_ylabel = 'd''';
+    bw_colormap = 'gray';
+    bw_data = bdp_mean;
+    bw_errors = bdp_sem;
+    h = barweb(bw_data,bw_errors,[],bw_groupnames,bw_title,bw_xlabel,bw_ylabel,bw_colormap,[],bw_legend);
+    set(h.legend,'Location','NorthEast');
+    axis([0.5 3.5 0 3]);
+    publishfig(gcf,0);
+  end
+end
+
+%% image manipulation conditions
+
+sessions = {'pretest', 'posttest', 'posttest_delay'};
+phases = {'match_1'};
+training = {'trained','untrained'};
+naming = {'basic','subord'};
+imgConds = {'normal','color','g','g_hi8','g_lo8'};
+
+dataField = 'dp';
+
+dp = struct;
+for n = 1:length(naming)
+  dp.(naming{n}) = nan(length(subjects),length(sessions),length(phases),length(training),length(imgConds));
+end
+
+for s = 1:length(sessions)
+  sesName = sessions{s};
+  for p = 1:length(phases)
+    for t = 1:length(training)
+      for i = 1:length(imgConds)
+        for n = 1:length(naming)
+          dp.(naming{n})(:,s,p,t,i) = results.(sesName).(phases{p}).(training{t}).(imgConds{i}).(naming{n}).(dataField);
+        end
+      end
+    end
+  end
+end
+
+for i = 1:length(imgConds)
+  for p = 1:length(phases)
+    for n = 1:length(naming)
+      figure
+      bdp_mean = nan(length(sessions),length(training));
+      bdp_sem = nan(length(sessions),length(training));
+      
+      for t = 1:length(training)
+        for s = 1:length(sessions)
+          bdp_mean(s,t) = nanmean(dp.(naming{n})(:,s,p,t,i),1);
+          bdp_sem(s,t) = nanstd(dp.(naming{n})(:,s,p,t,i),1) ./ sqrt(sum(~isnan(dp.(naming{n})(:,s,p,t,i))));
+        end
+      end
+      
+      bw_title = sprintf('%s%s: %s: %s',upper(naming{n}(1)),naming{n}(2:end),strrep(phases{p},'_','\_'),strrep(imgConds{i},'_','\_'));
+      bw_groupnames = {'Pretest', 'Posttest', 'One week later'};
+      bw_legend = {'Trained','Untrained'};
+      %bw_xlabel = 'Test day';
+      bw_xlabel = [];
+      bw_ylabel = 'd''';
+      bw_colormap = 'gray';
+      bw_data = bdp_mean;
+      bw_errors = bdp_sem;
+      h = barweb(bw_data,bw_errors,[],bw_groupnames,bw_title,bw_xlabel,bw_ylabel,bw_colormap,[],bw_legend);
+      set(h.legend,'Location','NorthEast');
+      axis([0.5 3.5 0 4]);
+      publishfig(gcf,0);
+    end
+  end
+end
+
+
