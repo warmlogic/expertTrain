9c3efe2199173afdce9af6e3482e67876c9ff67f
Matt Mollison <matt.mollison@gmail.com>
2013-07-31 13:00:39 -0600

    Windows batch file

---
+++ runEBIRD.bat
@@ -0,0 +1,5 @@
+REM This file should be located in the experiment files directory.
+REM Save this file as NAME.bat.
+REM Create a shortcut, move to somewhere convenient, double-click to run.
+matlab -sd "C:\Documents and Settings\curranlab\My Documents\My Experiments\expertTrain" -r "expertTrain"
+
