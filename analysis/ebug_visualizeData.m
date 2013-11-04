282d767e5628d3d23ddbd4d841ec63091ec29090
Shane <shane.schwikert@colorado.edu>
2013-11-04 15:12:12 -0700

    EBUG debugging

--- analysis/ebug_visualizeData.m
+++ analysis/ebug_visualizeData.m
@@ -24,7 +24,7 @@ end
 subjects = {
 %  'EBUG001';
    'EBUG002';
-   'EBUG003';
+%  'EBUG003';
    'EBUG004';
    'EBUG005';
   };
@@ -45,14 +45,15 @@ end
 
 onlyCompleteSub = true;
 printResults = true;
-saveResults = false;
+saveResults = true;
 
 [results] = ebug_processData(dataroot,subjects,onlyCompleteSub,printResults,saveResults);
 
 %% Plot basic and subordinate RTs across training days, all phases on one figure
 
 nTrainSes = 6;
-phases = {'name_1','name_2','name_3','name_4'};
+% phases = {'name_1','name_2','name_3','name_4'};
+phases = {'name_1'};
 
 data.overall = nan(length(subjects),(nTrainSes * length(phases) - 2));
 data.basic = nan(length(subjects),(nTrainSes * length(phases) - 2));
@@ -60,8 +61,8 @@ data.subord = nan(length(subjects),(nTrainSes * length(phases) - 2));
 
 % dataMeasure = 'rt';
 % dataMeasure = 'rt_cor';
-% dataMeasure = 'rt_inc';
-dataMeasure = 'acc';
+dataMeasure = 'rt_inc';
+% dataMeasure = 'acc';
 % dataMeasure = 'dp';
 
 tpCounter = 0;
@@ -98,7 +99,10 @@ xlabel('Training Day');
 if strcmp(dataMeasure,'rt') || strcmp(dataMeasure,'rt_cor') || strcmp(dataMeasure,'rt_inc')
   ylabel('Response Time (ms)');
   
-  axis([0.5 (size(data.subord,2) + 0.5) 800 round(max(data.overall(:))/100)*100 + 200]);
+%   axis([0.5 (size(data.subord,2) + 0.5) 800 round(max(data.overall(:))/100)*100 + 200]);
+  axis([0.5 (size(data.subord,2) + 0.5) 300 round(max(data.overall(:))/100)*100 + 600]);
+  
+  
   %if ~strcmp(dataMeasure,'rt_inc')
   %  axis([0.5 (size(data.subord,2) + 0.5) 0 600]);
   %else
@@ -109,15 +113,16 @@ if strcmp(dataMeasure,'rt') || strcmp(dataMeasure,'rt_cor') || strcmp(dataMeasur
 elseif strcmp(dataMeasure,'acc')
   ylabel('Accuracy');
   
-  %axis([0.5 (size(data.subord,2) + 0.5) 0.5 round(max(data.overall(:))*100)/100]);
-  axis([0.5 (size(data.subord,2) + 0.5) 0.5 1]);
+%   axis([0.5 (size(data.subord,2) + 0.5) 0.5 round(max(data.overall(:))*100)/100]);
+  axis([0.5 (size(data.subord,2) + 0.5) 0.4 1]);
   
   legendLoc = 'SouthEast';
 elseif strcmp(dataMeasure,'dp')
   ylabel('d''');
   
-  %axis([0.5 (size(data.subord,2) + 0.5) 0 ceil(max(data.overall(:)))]);
-  axis([0.5 (size(data.subord,2) + 0.5) 0 5]);
+%   axis([0.5 (size(data.subord,2) + 0.5) 0 ceil(max(data.overall(:)))]);
+%   axis([0.5 (size(data.subord,2) + 0.5) 0 5]);
+   axis([0.5 (size(data.subord,2) + 0.5) -1 6]);
   
   legendLoc = 'SouthEast';
 end
@@ -199,80 +204,81 @@ for p = 1:length(phases)
     end
   end
 end
+%%
 
-%% Image manipulation conditions: pretest, posttest, posttest_delay
-
-% plot basic and subordinate data for pretest, posttest, posttest_delay
-
-dataMeasure = 'dp';
-dataLabel = 'd''';
-ylimits = [0 4];
-
-% dataMeasure = 'acc';
-% dataLabel = 'Accuracy';
-% ylimits = [0 1];
-
-sessions = {'pretest', 'posttest', 'posttest_delay'};
-phases = {'match_1'};
-training = {'trained','untrained'};
-naming = {'basic','subord'};
-imgConds = {'normal','color','g','g_hi8','g_lo8'};
-
-data.(dataMeasure) = struct;
-for n = 1:length(naming)
-  data.(dataMeasure).(naming{n}) = nan(length(subjects),length(sessions),length(phases),length(training),length(imgConds));
-end
-
-for s = 1:length(sessions)
-  sesName = sessions{s};
-  for p = 1:length(phases)
-    for t = 1:length(training)
-      for i = 1:length(imgConds)
-        for n = 1:length(naming)
-          data.(dataMeasure).(naming{n})(:,s,p,t,i) = results.(sesName).(phases{p}).(training{t}).(imgConds{i}).(naming{n}).(dataMeasure);
-        end
-      end
-    end
-  end
-end
-
-% % stats
-% [h, p, ci, stats] = ttest(squeeze(data.(dataMeasure).(naming{1})(:,1,1,1,1)),squeeze(data.(dataMeasure).(naming{1})(:,2,1,1,1)));
-
-% make some plots
-for i = 1:length(imgConds)
-  for p = 1:length(phases)
-    for n = 1:length(naming)
-      figure
-      data_mean = nan(length(sessions),length(training));
-      data_sem = nan(length(sessions),length(training));
-      
-      for t = 1:length(training)
-        for s = 1:length(sessions)
-          data_mean(s,t) = nanmean(data.(dataMeasure).(naming{n})(:,s,p,t,i),1);
-          data_sem(s,t) = nanstd(data.(dataMeasure).(naming{n})(:,s,p,t,i),1) ./ sqrt(sum(~isnan(data.(dataMeasure).(naming{n})(:,s,p,t,i))));
-        end
-      end
-      
-      bw_title = sprintf('%s%s: %s: %s',upper(naming{n}(1)),naming{n}(2:end),strrep(phases{p},'_','\_'),strrep(imgConds{i},'_','\_'));
-      bw_groupnames = {'Pretest', 'Posttest', 'One week later'};
-      bw_legend = {'Trained','Untrained'};
-      %bw_xlabel = 'Test day';
-      bw_xlabel = [];
-      bw_ylabel = dataLabel;
-      bw_colormap = 'gray';
-      bw_data = data_mean;
-      bw_errors = data_sem;
-      h = barweb(bw_data,bw_errors,[],bw_groupnames,bw_title,bw_xlabel,bw_ylabel,bw_colormap,[],bw_legend,[],'plot');
-      set(h.legend,'Location','NorthWest');
-      axis([0.5 3.5 ylimits(1) ylimits(2)]);
-      publishfig(gcf,0);
-      if saveFigs
-        print(gcf,'-dpng',fullfile(figsDir,sprintf('prepost_trainUn_%s_%s_%s_%s',phases{p},dataMeasure,naming{n},imgConds{i})));
-      end
-    end
-  end
-end
+% %% Image manipulation conditions: pretest, posttest, posttest_delay
+% 
+% % plot basic and subordinate data for pretest, posttest, posttest_delay
+% 
+% dataMeasure = 'dp';
+% dataLabel = 'd''';
+% ylimits = [0 4];
+% 
+% % dataMeasure = 'acc';
+% % dataLabel = 'Accuracy';
+% % ylimits = [0 1];
+% 
+% sessions = {'pretest', 'posttest', 'posttest_delay'};
+% phases = {'match_1'};
+% training = {'trained','untrained'};
+% naming = {'basic','subord'};
+% imgConds = {'normal','color','g','g_hi8','g_lo8'};
+% 
+% data.(dataMeasure) = struct;
+% for n = 1:length(naming)
+%   data.(dataMeasure).(naming{n}) = nan(length(subjects),length(sessions),length(phases),length(training),length(imgConds));
+% end
+% 
+% for s = 1:length(sessions)
+%   sesName = sessions{s};
+%   for p = 1:length(phases)
+%     for t = 1:length(training)
+%       for i = 1:length(imgConds)
+%         for n = 1:length(naming)
+%           data.(dataMeasure).(naming{n})(:,s,p,t,i) = results.(sesName).(phases{p}).(training{t}).(imgConds{i}).(naming{n}).(dataMeasure);
+%         end
+%       end
+%     end
+%   end
+% end
+% 
+% % % stats
+% % [h, p, ci, stats] = ttest(squeeze(data.(dataMeasure).(naming{1})(:,1,1,1,1)),squeeze(data.(dataMeasure).(naming{1})(:,2,1,1,1)));
+% 
+% % make some plots
+% for i = 1:length(imgConds)
+%   for p = 1:length(phases)
+%     for n = 1:length(naming)
+%       figure
+%       data_mean = nan(length(sessions),length(training));
+%       data_sem = nan(length(sessions),length(training));
+%       
+%       for t = 1:length(training)
+%         for s = 1:length(sessions)
+%           data_mean(s,t) = nanmean(data.(dataMeasure).(naming{n})(:,s,p,t,i),1);
+%           data_sem(s,t) = nanstd(data.(dataMeasure).(naming{n})(:,s,p,t,i),1) ./ sqrt(sum(~isnan(data.(dataMeasure).(naming{n})(:,s,p,t,i))));
+%         end
+%       end
+%       
+%       bw_title = sprintf('%s%s: %s: %s',upper(naming{n}(1)),naming{n}(2:end),strrep(phases{p},'_','\_'),strrep(imgConds{i},'_','\_'));
+%       bw_groupnames = {'Pretest', 'Posttest', 'One week later'};
+%       bw_legend = {'Trained','Untrained'};
+%       %bw_xlabel = 'Test day';
+%       bw_xlabel = [];
+%       bw_ylabel = dataLabel;
+%       bw_colormap = 'gray';
+%       bw_data = data_mean;
+%       bw_errors = data_sem;
+%       h = barweb(bw_data,bw_errors,[],bw_groupnames,bw_title,bw_xlabel,bw_ylabel,bw_colormap,[],bw_legend,[],'plot');
+%       set(h.legend,'Location','NorthWest');
+%       axis([0.5 3.5 ylimits(1) ylimits(2)]);
+%       publishfig(gcf,0);
+%       if saveFigs
+%         print(gcf,'-dpng',fullfile(figsDir,sprintf('prepost_trainUn_%s_%s_%s_%s',phases{p},dataMeasure,naming{n},imgConds{i})));
+%       end
+%     end
+%   end
+% end
 
 %% old stuff
 
