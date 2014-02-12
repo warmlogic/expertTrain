1ec7087cdfdf4ae7d00cecb3354c2a5b3e08c858
Matt Mollison <matt.mollison@gmail.com>
2014-02-11 18:47:25 -0700

    removed prep_eeg

--- analysis/ebird_prepData_events.m
+++ analysis/ebird_prepData_events.m
@@ -1,25 +1,18 @@
-function ebird_prepData_events(subjects,prep_eeg)
-% ebird_prepData_events(subjects,prep_eeg)
+function ebird_prepData_events(subjects)
+% ebird_prepData_events(subjects)
 %
 % Purpose
-%   Create behavioral events; if prep_eeg == 1: export Net Station events
+%   Create behavioral events
 %
 % Inputs
 %   subjects: a cell of subject numbers
-%
-%   prep_eeg: boolean, whether or not to prepare the EEG data
 %
 % Outputs
 %   Events (struct and NetStation) will be saved in:
 %     ~/data/EBIRD/Behavioral/Sessions/subject/events
 %
-% Assumptions
-%   Each subject ran in both sessions (session_0 and session_1)
-%
 %   The behavioral data is located in:
 %     ~/data/EBIRD/Behavioral/Sessions/subject/session
-%
-% NB: EEG processing and NS event creation are not done yet
 %
 
 expName = 'EBIRD';
@@ -38,9 +31,13 @@ else
 end
 saveDir = dataroot;
 
+if ~exist('prep_eeg','var') || isempty(prep_eeg)
+  prep_eeg = false;
+end
+
 % DNF = "Did not finish" some number of sessions
 
-if nargin == 0
+if ~exist('subjects','var') || isempty(subjects)
   subjects = {
     'EBIRD049'; % Pilot. (due to short ses1 match, missing ses2 name)
     'EBIRD002'; % Pilot. (due to short ses1 match, missing ses2 name)
@@ -66,15 +63,13 @@ if nargin == 0
     'EBIRD022'; % DNF. Dropout. Last session: 8.
     'EBIRD023'; % DNF. Dropout. Last session: 1.
     'EBIRD024';
-    'EBIRD025'; % In progress. Last session: 1.
-    'EBIRD027'; % In progress. Last session: 8. Finishing week of 1/27.
-    'EBIRD029'; % In progress. Last session: 1.
+    'EBIRD025'; % In progress. Last session: 8.
+    'EBIRD027';
+    'EBIRD029'; % In progress. Last session: 8.
     'EBIRD032';
     'EBIRD034';
-    'EBIRD042'; % In progress. Last session: 2.
+    'EBIRD042'; % In progress. Last session: 8.
     };
-  
-  prep_eeg = 0;
 end
 
 %sessions = {'session_0','session_1'};
@@ -84,15 +79,15 @@ end
 for sub = 1:length(subjects)
   fprintf('Getting data for %s...\n',subjects{sub});
   
-  if prep_eeg == 1
-    % find the bad channels for this subject and session
-    %       sesStruct = filterStruct(infoStruct,'ismember(subject,varargin{1}) & ismember(session,varargin{2})',subjects{sub},sessions{ses});
-    %       subSesBadChan = sesStruct.badChan;
-    %       if isempty(sesStruct)
-    %         error('no subject listing found for this session');
-    %       end
-    subSesBadChan = [];
-  end
+%   if prep_eeg
+%     % find the bad channels for this subject and session
+%     %       sesStruct = filterStruct(infoStruct,'ismember(subject,varargin{1}) & ismember(session,varargin{2})',subjects{sub},sessions{ses});
+%     %       subSesBadChan = sesStruct.badChan;
+%     %       if isempty(sesStruct)
+%     %         error('no subject listing found for this session');
+%     %       end
+%     subSesBadChan = [];
+%   end
   
   % set the subject events directory
   eventsOutdir_sub = fullfile(saveDir,subjects{sub},'events');
@@ -156,39 +151,39 @@ for sub = 1:length(subjects)
   %saveEvents(events,eventsOutfile_sub);
   fprintf('Done.\n');
   
-  %% prep the EEG data
-  if prep_eeg == 1
-    for sesNum = 1:length(expParam.sesTypes)
-      fprintf('Prepping EEG data for %s...\n',expParam.sesTypes{sesNum});
-      % get this subject's session dir
-      subDir = fullfile(dataroot,subjects{sub});
-      sesDir = fullfile(subDir,sprintf('session_%d',sesNum));
-      
-      subEegDir = fullfile(sesDir,'eeg','eeg.noreref');
-      pfile = dir(fullfile(subEegDir,[subjects{sub},'*params.txt']));
-      
-      if ~exist(fullfile(subEegDir,pfile.name),'file')
-        curDir = pwd;
-        % cd to the session directory since prep_egi_data needs to be there
-        cd(sesDir);
-        % align the behavioral and EEG data
-        %
-        % TODO: make sure this works
-        prep_egi_data_CU(subjects{sub},sesDir,{fullfile(subDir,'events',sprintf('events_ses%d.mat',sesNum))},subSesBadChan,'mstime','HCGSN');
-        % go back to the previous working directory
-        cd(curDir);
-      end
-      
-      % export the events for netstation; saves to the session's events dir
-      %
-      % TODO: create/fix ebird_events2ns so it doesn't rely on events in
-      % session directory
-      fprintf('ebird_events2ns IS NOT DONE YET!!!\n');
-      ebird_events2ns(dataroot,subjects{sub},sesNum);
-      
-    end % ses
-    
-  end % prep_eeg
+%   %% prep the EEG data
+%   if prep_eeg
+%     for sesNum = 1:length(expParam.sesTypes)
+%       fprintf('Prepping EEG data for %s...\n',expParam.sesTypes{sesNum});
+%       % get this subject's session dir
+%       subDir = fullfile(dataroot,subjects{sub});
+%       sesDir = fullfile(subDir,sprintf('session_%d',sesNum));
+%       
+%       subEegDir = fullfile(sesDir,'eeg','eeg.noreref');
+%       pfile = dir(fullfile(subEegDir,[subjects{sub},'*params.txt']));
+%       
+%       if ~exist(fullfile(subEegDir,pfile.name),'file')
+%         curDir = pwd;
+%         % cd to the session directory since prep_egi_data needs to be there
+%         cd(sesDir);
+%         % align the behavioral and EEG data
+%         %
+%         % TODO: make sure this works
+%         prep_egi_data_CU(subjects{sub},sesDir,{fullfile(subDir,'events',sprintf('events_ses%d.mat',sesNum))},subSesBadChan,'mstime','HCGSN');
+%         % go back to the previous working directory
+%         cd(curDir);
+%       end
+%       
+%       % export the events for netstation; saves to the session's events dir
+%       %
+%       % TODO: create/fix ebird_events2ns so it doesn't rely on events in
+%       % session directory
+%       fprintf('ebird_events2ns IS NOT DONE YET!!!\n');
+%       ebird_events2ns(dataroot,subjects{sub},sesNum);
+%       
+%     end % ses
+%   end % prep_eeg
+  
   fprintf('Done processing %s.\n',subjects{sub});
 end % sub
 
--- analysis/ebug_prepData_events.m
+++ analysis/ebug_prepData_events.m
@@ -1,25 +1,18 @@
-function ebug_prepData_events(subjects,prep_eeg)
-% ebug_prepData_events(subjects,prep_eeg)
+function ebug_prepData_events(subjects)
+% ebug_prepData_events(subjects)
 %
 % Purpose
-%   Create behavioral events; if prep_eeg == 1: export Net Station events
+%   Create behavioral events
 %
 % Inputs
 %   subjects: a cell of subject numbers
 %
-%   prep_eeg: boolean, whether or not to prepare the EEG data
-%
 % Outputs
-%   Events (struct and NetStation) will be saved in:
+%   Events (struct) will be saved in:
 %     ~/data/EBUG/Behavioral/Sessions/subject/events
-%
-% Assumptions
-%   Each subject ran in both sessions (session_0 and session_1)
 %
 %   The behavioral data is located in:
 %     ~/data/EBUG/Behavioral/Sessions/subject/session
-%
-% NB: EEG processing and NS event creation are not done yet
 %
 
 expName = 'EBUG';
@@ -38,7 +31,13 @@ else
 end
 saveDir = dataroot;
 
-if nargin == 0
+% if ~exist('prep_eeg','var') || isempty(prep_eeg)
+%   prep_eeg = false;
+% end
+
+% DNF = "Did not finish" some number of sessions
+
+if ~exist('subjects','var') || isempty(subjects)
   subjects = {
 %     'EBUG001';
 %     'EBUG002';
@@ -56,8 +55,6 @@ if nargin == 0
 %     'EBUG094';
 %     'EBUG095';
     };
-  
-  prep_eeg = 0;
 end
 
 %sessions = {'session_0','session_1'};
@@ -67,15 +64,15 @@ end
 for sub = 1:length(subjects)
   fprintf('Getting data for %s...\n',subjects{sub});
   
-  if prep_eeg == 1
-    % find the bad channels for this subject and session
-    %       sesStruct = filterStruct(infoStruct,'ismember(subject,varargin{1}) & ismember(session,varargin{2})',subjects{sub},sessions{ses});
-    %       subSesBadChan = sesStruct.badChan;
-    %       if isempty(sesStruct)
-    %         error('no subject listing found for this session');
-    %       end
-    subSesBadChan = [];
-  end
+%   if prep_eeg
+%     % find the bad channels for this subject and session
+%     %       sesStruct = filterStruct(infoStruct,'ismember(subject,varargin{1}) & ismember(session,varargin{2})',subjects{sub},sessions{ses});
+%     %       subSesBadChan = sesStruct.badChan;
+%     %       if isempty(sesStruct)
+%     %         error('no subject listing found for this session');
+%     %       end
+%     subSesBadChan = [];
+%   end
   
   % set the subject events directory
   eventsOutdir_sub = fullfile(saveDir,subjects{sub},'events');
@@ -139,39 +136,39 @@ for sub = 1:length(subjects)
   %saveEvents(events,eventsOutfile_sub);
   fprintf('Done.\n');
   
-  %% prep the EEG data
-  if prep_eeg == 1
-    for sesNum = 1:length(expParam.sesTypes)
-      fprintf('Prepping EEG data for %s...\n',expParam.sesTypes{sesNum});
-      % get this subject's session dir
-      subDir = fullfile(dataroot,subjects{sub});
-      sesDir = fullfile(subDir,sprintf('session_%d',sesNum));
-      
-      subEegDir = fullfile(sesDir,'eeg','eeg.noreref');
-      pfile = dir(fullfile(subEegDir,[subjects{sub},'*params.txt']));
-      
-      if ~exist(fullfile(subEegDir,pfile.name),'file')
-        curDir = pwd;
-        % cd to the session directory since prep_egi_data needs to be there
-        cd(sesDir);
-        % align the behavioral and EEG data
-        %
-        % TODO: make sure this works
-        prep_egi_data_CU(subjects{sub},sesDir,{fullfile(subDir,'events',sprintf('events_ses%d.mat',sesNum))},subSesBadChan,'mstime','HCGSN');
-        % go back to the previous working directory
-        cd(curDir);
-      end
-      
-      % export the events for netstation; saves to the session's events dir
-      %
-      % TODO: create/fix ebug_events2ns so it doesn't rely on events in
-      % session directory
-      fprintf('ebug_events2ns IS NOT DONE YET!!!\n');
-      ebug_events2ns(dataroot,subjects{sub},sesNum);
-      
-    end % ses
-    
-  end % prep_eeg
+%   %% prep the EEG data
+%   if prep_eeg
+%     for sesNum = 1:length(expParam.sesTypes)
+%       fprintf('Prepping EEG data for %s...\n',expParam.sesTypes{sesNum});
+%       % get this subject's session dir
+%       subDir = fullfile(dataroot,subjects{sub});
+%       sesDir = fullfile(subDir,sprintf('session_%d',sesNum));
+%       
+%       subEegDir = fullfile(sesDir,'eeg','eeg.noreref');
+%       pfile = dir(fullfile(subEegDir,[subjects{sub},'*params.txt']));
+%       
+%       if ~exist(fullfile(subEegDir,pfile.name),'file')
+%         curDir = pwd;
+%         % cd to the session directory since prep_egi_data needs to be there
+%         cd(sesDir);
+%         % align the behavioral and EEG data
+%         %
+%         % TODO: make sure this works
+%         prep_egi_data_CU(subjects{sub},sesDir,{fullfile(subDir,'events',sprintf('events_ses%d.mat',sesNum))},subSesBadChan,'mstime','HCGSN');
+%         % go back to the previous working directory
+%         cd(curDir);
+%       end
+%       
+%       % export the events for netstation; saves to the session's events dir
+%       %
+%       % TODO: create/fix ebug_events2ns so it doesn't rely on events in
+%       % session directory
+%       fprintf('ebug_events2ns IS NOT DONE YET!!!\n');
+%       ebug_events2ns(dataroot,subjects{sub},sesNum);
+%       
+%     end % ses
+%   end % prep_eeg
+  
   fprintf('Done processing %s.\n',subjects{sub});
 end % sub
 
