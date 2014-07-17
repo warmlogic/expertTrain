beafb88b1e0c995ba6ae1263a81db06b96cb6115
Hillary Hadley <BCDlab@psych.umass.edu>
2014-07-17 12:24:53 -0400

    NSHost fix

--- config_EBUG_UMA.m
+++ config_EBUG_UMA.m
@@ -23,7 +23,7 @@ expParam.useNS
   % expParam.NSHost = '128.138.223.251';
   
   % D464
-  expParam.NSHost = '10.0.0.41';
+  expParam.NSHost = '10.0.0.42';
   
   expParam.baselineRecordSecs = 20.0;
 
