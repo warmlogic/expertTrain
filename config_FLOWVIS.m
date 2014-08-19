8344ec357cd0801576256c12eb7b0a935508238f
KatherineG <katherine.goodman@colorado.edu>
2014-08-19 08:23:03 -0600

    Name with feedback
    changed IsExp on name phase to false

--- config_FLOWVIS.m
+++ config_FLOWVIS.m
@@ -621,7 +621,7 @@ if expParam.sessionNum == 1
     
     if ismember(phaseName,expParam.session.(sesName).phases)
       for phaseCount = 1:sum(ismember(expParam.session.(sesName).phases,phaseName))
-        cfg.stim.(sesName).(phaseName)(phaseCount).isExp = true;
+        cfg.stim.(sesName).(phaseName)(phaseCount).isExp = false;
         cfg.stim.(sesName).(phaseName)(phaseCount).impedanceBeforePhase = false;
         cfg.stim.(sesName).(phaseName)(phaseCount).respDuringStim = true;
         
