37e69e20e00d8c61cffc9489000d38d8ed5c81d2
Matt Mollison <matt.mollison@gmail.com>
2014-08-26 17:44:13 -0600

    support for zero lures

--- space_processStims_test.m
+++ space_processStims_test.m
@@ -61,39 +61,43 @@ expParam.session.(sesName).(phaseName)(phaseCount).testStims_word = [];
 if ~phaseCfg.isExp
   % for the practice
   
-  % put all the practice image stimuli together, across categories
-  for cn = 1:length(cfg.stim.practice.categoryNames)
-    if ismember(cfg.stim.practice.categoryNames{cn},phaseCfg.categoryNames)
-      [testStims_img,imgStimStruct(cn).catStims] = space_divvyStims(...
-        imgStimStruct(cn).catStims,testStims_img,...
-        cfg.stim.practice.nPairs_test_lure,...
-        cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{true,false,false,-1,[],[],[]});
+  if cfg.stim.practice.nPairs_test_lure > 0
+    % put all the practice image stimuli together, across categories
+    for cn = 1:length(cfg.stim.practice.categoryNames)
+      if ismember(cfg.stim.practice.categoryNames{cn},phaseCfg.categoryNames)
+        [testStims_img,imgStimStruct(cn).catStims] = space_divvyStims(...
+          imgStimStruct(cn).catStims,testStims_img,...
+          cfg.stim.practice.nPairs_test_lure,...
+          cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{true,false,false,-1,[],[],[]});
+      end
     end
+    
+    % do the word stimuli
+    [testStims_word,wordStimStruct.wordStims] = space_divvyStims(...
+      wordStimStruct.wordStims,testStims_word,...
+      cfg.stim.practice.nPairs_test_lure * length(phaseCfg.categoryNames),...
+      cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{true,false,false,-1,[],[],[]});
   end
-  
-  % do the word stimuli
-  [testStims_word,wordStimStruct.wordStims] = space_divvyStims(...
-    wordStimStruct.wordStims,testStims_word,...
-    cfg.stim.practice.nPairs_test_lure * length(phaseCfg.categoryNames),...
-    cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{true,false,false,-1,[],[],[]});
 else
   % for the real experiment
   
-  % put all the image stimuli together, across categories
-  for cn = 1:length(cfg.stim.categoryNames)
-    if ismember(cfg.stim.categoryNames{cn},phaseCfg.categoryNames)
-      [testStims_img,imgStimStruct(cn).catStims] = space_divvyStims(...
-        imgStimStruct(cn).catStims,testStims_img,...
-        cfg.stim.nPairs_test_lure,...
-        cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{false,false,false,-1,[],[],[]});
+  if cfg.stim.nPairs_test_lure > 0
+    % put all the image stimuli together, across categories
+    for cn = 1:length(cfg.stim.categoryNames)
+      if ismember(cfg.stim.categoryNames{cn},phaseCfg.categoryNames)
+        [testStims_img,imgStimStruct(cn).catStims] = space_divvyStims(...
+          imgStimStruct(cn).catStims,testStims_img,...
+          cfg.stim.nPairs_test_lure,...
+          cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{false,false,false,-1,[],[],[]});
+      end
     end
+    
+    % do the word stimuli
+    [testStims_word,wordStimStruct.wordStims] = space_divvyStims(...
+      wordStimStruct.wordStims,testStims_word,...
+      cfg.stim.nPairs_test_lure * length(phaseCfg.categoryNames),...
+      cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{false,false,false,-1,[],[],[]});
   end
-  
-  % do the word stimuli
-  [testStims_word,wordStimStruct.wordStims] = space_divvyStims(...
-    wordStimStruct.wordStims,testStims_word,...
-    cfg.stim.nPairs_test_lure * length(phaseCfg.categoryNames),...
-    cfg.stim.rmStims_init,cfg.stim.shuffleFirst_init,{'practice','targ','spaced','lag','presNum','pairNum','pairOrd'},{false,false,false,-1,[],[],[]});
 end
 
 % get the study target stimuli
@@ -125,22 +129,31 @@ if testInOrderedGroups > 1
   lureStimsInFinalGroup = mod(length(testStims_img),testInOrderedGroups);
   
   % shuffle the lures before we select from them
-  fprintf('Shuffling %s test (%d) task stimuli, lures for ordered groups.\n',sesName,phaseCount);
-  [testStims_img,randind] = et_shuffleStims(...
-    testStims_img,'categoryNum',phaseCfg.crMaxConsecCategory);
-  % put the word stimuli in the same shuffled order
-  testStims_word = testStims_word(randind);
+  if ~isempty(testStims_img)
+    fprintf('Shuffling %s test (%d) task stimuli, lures for ordered groups.\n',sesName,phaseCount);
+    [testStims_img,randind] = et_shuffleStims(...
+      testStims_img,'categoryNum',phaseCfg.crMaxConsecCategory);
+    % put the word stimuli in the same shuffled order
+    testStims_word = testStims_word(randind);
+  end
   
   for i = 1:(testInOrderedGroups)
     % set the indices for this group
     targStartInd = (i * targStimsPerGroup) - targStimsPerGroup + 1;
     targEndInd = (i * targStimsPerGroup);
-    lureStartInd = (i * lureStimsPerGroup) - lureStimsPerGroup + 1;
-    lureEndInd = (i * lureStimsPerGroup);
+    if lureStimsPerGroup > 0
+      lureStartInd = (i * lureStimsPerGroup) - lureStimsPerGroup + 1;
+      lureEndInd = (i * lureStimsPerGroup);
+    end
     
     % put this group of targets and lures together
-    thisOrderedGroup_img = cat(1,p1_StudyStims_img(targStartInd:targEndInd),testStims_img(lureStartInd:lureEndInd));
-    thisOrderedGroup_word = cat(1,p1_StudyStims_word(targStartInd:targEndInd),testStims_word(lureStartInd:lureEndInd));
+    if ~isempty(testStims_img)
+      thisOrderedGroup_img = cat(1,p1_StudyStims_img(targStartInd:targEndInd),testStims_img(lureStartInd:lureEndInd));
+      thisOrderedGroup_word = cat(1,p1_StudyStims_word(targStartInd:targEndInd),testStims_word(lureStartInd:lureEndInd));
+    else
+      thisOrderedGroup_img = p1_StudyStims_img(targStartInd:targEndInd);
+      thisOrderedGroup_word = p1_StudyStims_word(targStartInd:targEndInd);
+    end
     
     % shuffle this group of targets and lures together
     fprintf('Shuffling %s test (%d) task stimuli, ordered test groups.\n',sesName,phaseCount);
@@ -152,14 +165,16 @@ if testInOrderedGroups > 1
     % concatenate targets and lures from this group, initialize if needed
     if ~exist('orderedGroups_img','var')
       orderedGroups_img = thisOrderedGroup_img;
-    else
-      orderedGroups_img = cat(1,orderedGroups_img,thisOrderedGroup_img);
-    end
-    if ~exist('orderedGroups_word','var')
       orderedGroups_word = thisOrderedGroup_word;
     else
+      orderedGroups_img = cat(1,orderedGroups_img,thisOrderedGroup_img);
       orderedGroups_word = cat(1,orderedGroups_word,thisOrderedGroup_word);
     end
+    %if ~exist('orderedGroups_word','var')
+    %  orderedGroups_word = thisOrderedGroup_word;
+    %else
+    %  orderedGroups_word = cat(1,orderedGroups_word,thisOrderedGroup_word);
+    %end
   end
   
   % do the final group if there were remainders
